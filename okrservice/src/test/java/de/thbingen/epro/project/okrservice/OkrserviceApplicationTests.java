449177f1afd1e43c3e950cd8a5bcc32434c1f855
Daezu <niklas.daezu@gmail.com>
2024-02-22 19:15:00 +0100

    Enabled testing on build

--- okrservice/src/test/java/de/thbingen/epro/project/okrservice/OkrserviceApplicationTests.java
+++ okrservice/src/test/java/de/thbingen/epro/project/okrservice/OkrserviceApplicationTests.java
@@ -1,13 +1,8 @@
 package de.thbingen.epro.project.okrservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OkrserviceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
